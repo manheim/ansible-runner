81bd5614cecca97975eb554c39edc07a81ded6cf
Marcus Smith <qwcode@gmail.com>
2018-12-16 16:48:17 -0800

    initial draft

---
+++ README.md
@@ -0,0 +1,20 @@
+# ansible_runner
+
+Shell script to install ansible in a virtual environment and run a playbook
+
+
+## Usage
+
+```
+###############################################################################################
+## ansible_runner.sh [<requirements file>] [<playbook file>] [<ansible version>] [<python exe>]
+##
+##    requirements file:  path to role requirements file.  defaults to 'requirements.yml'
+##    playbook file:  path to playbook file.  defaults to 'playbook.yml'
+##    ansible version:  ansible version. defaults to latest version on pypi
+##    python exe:  path to python exe. defaults to 'python'
+###############################################################################################
+
+```
+
+## Example
---
+++ ansible-runner.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python
+
+import glob
+import os
+import shutil
+from optparse import OptionParser
+from subprocess import call
+
+DEFAULT_VIRTUALENV_VERSION = '16.1.0'
+DEFAULT_ANSIBLE_REQ = 'ansible'
+
+def run():
+    parser = OptionParser()
+    parser.add_option('-r', '--requirements', dest='requirements', default='build-requirements.yml',
+                      help='Path to ansible galaxy requirements file. Defaults to "build-requirements.yml"' )
+    parser.add_option('-p', '--playbook', dest='playbook', default='build-playbook.yml',
+                      help='Path to playbook file. Defaults to "build-playbook.yml"' )
+    parser.add_option('-a', '--ansible-requirement', dest='ansible_req',  default=DEFAULT_ANSIBLE_REQ,
+                      help='The pip install ansible requirement. Defaults to "%s"' % DEFAULT_ANSIBLE_REQ )
+    parser.add_option('-e', '--python-exe', dest='python_exe', default='python',
+                      help='Path to python exe. Defaults to "python"' )
+    parser.add_option('-i', '--install-dir', dest='install_dir', default='.ansible-runner',
+                      help='Install dir for ansible virtual environment. Defaults to ".ansible-runner"' )
+    parser.add_option('-v', '--virtualenv-version', dest='venv_version', default=DEFAULT_VIRTUALENV_VERSION,
+                      help='Virtualenv version to use. Defaults to "%s"' % DEFAULT_VIRTUALENV_VERSION)
+    parser.add_option('-c', '--clean', dest='clean', action='store_true',
+                      help='Clean the install dir, if it exists.' )
+    (options, args) = parser.parse_args()
+
+    if options.clean and os.path.isdir(options.install_dir):
+        shutil.rmtree(options.install_dir)
+
+    if not os.path.isdir(options.install_dir):
+        os.makedirs(options.install_dir)
+
+    venv_archive_file = 'virtualenv-%s.tar.gz' % options.venv_version
+    venv_archive_path = '%s/%s' % (options.install_dir, venv_archive_file)
+    venv_archive_url = 'https://github.com/pypa/virtualenv/tarball/%s' % options.venv_version
+    if not os.path.isfile(venv_archive_path):
+        call(['curl', '--location', '--output', venv_archive_path, venv_archive_url])
+
+    call(['tar', 'xvfz', venv_archive_path, '-C', options.install_dir])
+
+    venv_unpack_dir = glob.glob('%s/pypa-virtualenv-*' % options.install_dir)[0]
+    venv_dir = '%s/VE' % options.install_dir
+
+    if not os.path.isdir(venv_dir):
+        call([options.python_exe, '%s/src/virtualenv.py' % venv_unpack_dir, venv_dir])
+
+    call(['%s/bin/pip' % venv_dir, 'install', options.ansible_req])
+    call(['%s/bin/ansible-galaxy' % venv_dir, 'install', '-r', options.requirements])
+    call(['%s/bin/ansible-playbook' % venv_dir, options.playbook])
+
+
+if __name__ == '__main__':
+    run()
